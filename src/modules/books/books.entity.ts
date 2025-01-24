484778bfc729444a98aa3dc885507d31f854ed1b
Gregory Pereverzev <boot762@gmail.com>
2025-01-24 14:01:49 +0300

    feat: add books.entity.ts

---
+++ src/modules/books/books.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity } from 'src/core/entity/base.entity';
+
+@Entity('books')
+export class Book extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  author: string;
+
+  @Column()
+  ageRestriction: number;
+
+  @Column({ nullable: true })
+  ownerId: number;
+
+  @Column({ nullable: true })
+  image?: string;
+}
