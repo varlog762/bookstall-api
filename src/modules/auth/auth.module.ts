8a3384d053ed15e2300b0e7156fba7f66747d366
Gregory Pereverzev <boot762@gmail.com>
2025-01-27 14:03:07 +0300

    feat: import PassportModule and JwtModule to the AuthModule

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
 @Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: 'secret_key',
+      signOptions: {
+        expiresIn: '60m',
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
