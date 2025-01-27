aa57ea01cde6f593749a3f648ae4f99f96285d32
Gregory Pereverzev <boot762@gmail.com>
2025-01-27 19:48:29 +0300

    feat: register JwtStrategy in BooksModule

--- src/modules/books/books.module.ts
+++ src/modules/books/books.module.ts
@@ -4,10 +4,11 @@ import { BooksController } from './books.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BooksRepository } from './books.repository';
 import { Book } from './book.entity';
+import { JwtStrategy } from 'src/core/guards/jwt.strategy';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Book])],
-  providers: [BooksService, BooksRepository],
+  providers: [BooksService, BooksRepository, JwtStrategy],
   controllers: [BooksController],
 })
 export class BooksModule {}
