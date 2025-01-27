fe3b15dfc5f2ee06c8c72b1db92c21428c5506b3
Gregory Pereverzev <boot762@gmail.com>
2025-01-27 15:29:34 +0300

    feat: add login method to AuthService

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,11 +1,23 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { UsersRepository } from '../users/users.repository';
+import { JwtService } from '@nestjs/jwt';
 import bcrypt from 'bcrypt';
+
+import { UsersRepository } from '../users/users.repository';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersRepository: UsersRepository) {}
+  constructor(
+    private usersRepository: UsersRepository,
+    private jwtService: JwtService,
+  ) {}
 
+  /**
+   * Validate a user by their email and password.
+   * @param email The user's email address.
+   * @param password The user's password.
+   * @returns The user's ID.
+   * @throws UnauthorizedException If the user is not valid.
+   */
   async validateUser(
     email: string,
     password: string,
@@ -25,5 +37,16 @@ export class AuthService {
     return {
       userId: user.id,
     };
+  }
+
+  /**
+   * Create a JWT token for the given user ID.
+   * @param userId The ID of the user to be logged in.
+   * @returns An object containing the JWT token.
+   */
+  login(userId: string): { accessToken: string } {
+    const accessToken = this.jwtService.sign(userId);
+
+    return { accessToken };
   }
 }
